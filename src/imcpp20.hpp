7ec1c2cca93395e97dc4fc07a7fa651176127406
Ivan Sokolov <sokolovivanl2rwe@yandex.ru>
2023-04-07 15:44:28 +0300

    make imcpp20.hpp build with clang

--- src/imcpp20.hpp
+++ src/imcpp20.hpp
@@ -17,13 +17,20 @@ namespace ImScoped {
 //
 // The pairing End* function is called automatically if needed.
 // Note the `auto w = ...` Naming the object is necessary for it to live long enough
-#define IM_SCOPED_LAMBDA_WRAP(F) [](auto&&... wrap_arg){ return F(std::forward<decltype(wrap_arg)>(wrap_arg)...); }
+
+// Widget types resort to a lambda that wraps the function to properly forward arguments into
+// functions with default arguments (most ImGui functions) and/or overloaded functions (some),
+// and detail::Widget takes that lambda as a NTTP, meaning that none of the ImScoped:: types
+// can ever cross any ABI.
+// That is fine, given the usage; these types should only ever be local variables.
+// (Wrapping with the lambda inside detail::Widget::Widget() rather than outside it fixes
+//  the ABI issue, but only GCC compiles this, and it doesn't resolve overloaded functions.)
 
 namespace detail {
 template <auto BeginF, auto EndF, bool AlwaysCallEndF = false> class Widget {
 	bool Shown;
 public:
-	explicit Widget (auto&&... a): Shown{ IM_SCOPED_LAMBDA_WRAP(BeginF)(std::forward<decltype(a)>(a)...) } {}
+	explicit Widget (auto&&... a): Shown{ BeginF(std::forward<decltype(a)>(a)...) } {}
 	~Widget () { if (AlwaysCallEndF || Shown) EndF(); }
 
 	explicit operator bool () const& { return Shown; }
@@ -38,41 +45,40 @@ public:
 	Widget& operator= (Widget&&) = delete;
 };
 } // namespace detail
+#define IM_WIDGET(BEGIN, ...) \
+	detail::Widget<[] (auto&&... args) { return BEGIN(std::forward<decltype(args)>(args)...); }, __VA_ARGS__>
 
-using Window = detail::Widget<ImGui::Begin, ImGui::End, true>;
+using Window = IM_WIDGET(ImGui::Begin, ImGui::End, true);
+using ChildWindow = IM_WIDGET(ImGui::BeginChild, ImGui::EndChild);
+using ChildFrame = IM_WIDGET(ImGui::BeginChildFrame, ImGui::EndChildFrame);
 
-// Because ImGui::BeginChild is, besides having defaulted arguments, overloaded, we can't pass it as `BeginF`,
-// so wrap it in a lambda here too. (Wrapping once is enough, but then we'd need a way to tell the constructor to not do it)
-// Using a lambda in a type name means we can never pass the object across any ABI boundary ever, but that is OK given the use
-using ChildWindow = detail::Widget<IM_SCOPED_LAMBDA_WRAP(ImGui::BeginChild), ImGui::EndChild, true>;
-using ChildFrame = detail::Widget<ImGui::BeginChildFrame, ImGui::EndChildFrame, true>;
+using Group = IM_WIDGET(ImGui::BeginGroup, ImGui::EndGroup);
 
-using Group = detail::Widget<ImGui::BeginGroup, ImGui::EndGroup>;
+using Table = IM_WIDGET(ImGui::BeginTable, ImGui::EndTable);
 
-using Table = detail::Widget<ImGui::BeginTable, ImGui::EndTable>;
+using TabBar = IM_WIDGET(ImGui::BeginTabBar, ImGui::EndTabBar);
+using TabItem = IM_WIDGET(ImGui::BeginTabItem, ImGui::EndTabItem);
 
-using TabBar = detail::Widget<ImGui::BeginTabBar, ImGui::EndTabBar>;
-using TabItem = detail::Widget<ImGui::BeginTabItem, ImGui::EndTabItem>;
+using MainMenuBar = IM_WIDGET(ImGui::BeginMainMenuBar, ImGui::EndMainMenuBar);
+using MenuBar = IM_WIDGET(ImGui::BeginMenuBar, ImGui::EndMenuBar);
+using Menu = IM_WIDGET(ImGui::BeginMenu, ImGui::EndMenu);
 
-using MainMenuBar = detail::Widget<ImGui::BeginMainMenuBar, ImGui::EndMainMenuBar>;
-using MenuBar = detail::Widget<ImGui::BeginMenuBar, ImGui::EndMenuBar>;
-using Menu = detail::Widget<ImGui::BeginMenu, ImGui::EndMenu>;
+using Combo = IM_WIDGET(ImGui::BeginCombo, ImGui::EndCombo);
+using ListBox = IM_WIDGET(ImGui::BeginListBox, ImGui::EndListBox);
+using Tooltip = IM_WIDGET(ImGui::BeginTooltip, ImGui::EndTooltip);
 
-using Combo = detail::Widget<ImGui::BeginCombo, ImGui::EndCombo>;
-using ListBox = detail::Widget<ImGui::BeginListBox, ImGui::EndListBox>;
-using Tooltip = detail::Widget<ImGui::BeginTooltip, ImGui::EndTooltip>;
+using Popup = IM_WIDGET(ImGui::BeginPopup, ImGui::EndPopup);
+using PopupModal = IM_WIDGET(ImGui::BeginPopupModal, ImGui::EndPopup);
 
-using Popup = detail::Widget<ImGui::BeginPopup, ImGui::EndPopup>;
-using PopupModal = detail::Widget<ImGui::BeginPopupModal, ImGui::EndPopup>;
+using DragDropSource = IM_WIDGET(ImGui::BeginDragDropSource, ImGui::EndDragDropSource);
+using DragDropTarget = IM_WIDGET(ImGui::BeginDragDropTarget, ImGui::EndDragDropTarget);
 
-using DragDropSource = detail::Widget<ImGui::BeginDragDropSource, ImGui::EndDragDropSource>;
-using DragDropTarget = detail::Widget<ImGui::BeginDragDropTarget, ImGui::EndDragDropTarget>;
-
-#undef IM_SCOPED_LAMBDA_WRAP
+#undef IM_WIDGET
 } // namespace ImScoped
 
 namespace ImGui {
 // =================================== ImGui x fmtlib ===================================
+// Incomplete, but the "real" ImGui API is very redundant anyway
 IMGUI_API void TextFmtV (fmt::string_view format, fmt::format_args);
 
 template <typename Fmt, typename... Args>
@@ -99,16 +105,16 @@ void TextFmtColored (const ImVec4& col, const Fmt& fmt, Args&&... args)
 
 // ===================== Get ImGuiDataType enum from an actual type =====================
 template <typename> constexpr ImGuiDataType DataTypeEnum = ImGuiDataType_COUNT;
-template<> constexpr ImGuiDataType DataTypeEnum<int8_t>   = ImGuiDataType_S8;
-template<> constexpr ImGuiDataType DataTypeEnum<uint8_t>  = ImGuiDataType_U8;
-template<> constexpr ImGuiDataType DataTypeEnum<int16_t>  = ImGuiDataType_S16;
-template<> constexpr ImGuiDataType DataTypeEnum<uint16_t> = ImGuiDataType_U16;
-template<> constexpr ImGuiDataType DataTypeEnum<int32_t>  = ImGuiDataType_S32;
-template<> constexpr ImGuiDataType DataTypeEnum<uint32_t> = ImGuiDataType_U32;
-template<> constexpr ImGuiDataType DataTypeEnum<int64_t>  = ImGuiDataType_S64;
-template<> constexpr ImGuiDataType DataTypeEnum<uint64_t> = ImGuiDataType_U64;
-template<> constexpr ImGuiDataType DataTypeEnum<float>    = ImGuiDataType_Float;
-template<> constexpr ImGuiDataType DataTypeEnum<double>   = ImGuiDataType_Double;
+template<> constexpr inline ImGuiDataType DataTypeEnum<int8_t>   = ImGuiDataType_S8;
+template<> constexpr inline ImGuiDataType DataTypeEnum<uint8_t>  = ImGuiDataType_U8;
+template<> constexpr inline ImGuiDataType DataTypeEnum<int16_t>  = ImGuiDataType_S16;
+template<> constexpr inline ImGuiDataType DataTypeEnum<uint16_t> = ImGuiDataType_U16;
+template<> constexpr inline ImGuiDataType DataTypeEnum<int32_t>  = ImGuiDataType_S32;
+template<> constexpr inline ImGuiDataType DataTypeEnum<uint32_t> = ImGuiDataType_U32;
+template<> constexpr inline ImGuiDataType DataTypeEnum<int64_t>  = ImGuiDataType_S64;
+template<> constexpr inline ImGuiDataType DataTypeEnum<uint64_t> = ImGuiDataType_U64;
+template<> constexpr inline ImGuiDataType DataTypeEnum<float>    = ImGuiDataType_Float;
+template<> constexpr inline ImGuiDataType DataTypeEnum<double>   = ImGuiDataType_Double;
 
 template <typename T> concept ScalarType = (DataTypeEnum<T> != ImGuiDataType_COUNT);
 
